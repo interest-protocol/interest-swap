991ffea8681d61610b92f2e3cead560c5f408efa
josemvcerqueira <jose@ilovemochi.com>
2022-05-17 17:54:14 +0100

    docs: add comments to fees contract

--- contracts/Fees.sol
+++ contracts/Fees.sol
@@ -3,9 +3,16 @@ pragma solidity 0.8.13;
 
 import "./interfaces/IERC20.sol";
 
+/**
+ *@dev This contract holds all tokens accrued by swap fees by a pair. Each Pair Contract creates a Fees Contract.
+ * Only the pair can transfer tokens in and out from it.
+ */
 contract Fees {
+    // The pair contract that created it.
     address private immutable pair;
+    // Token0 of the pair
     address private immutable token0;
+    // Token1 of the pair
     address private immutable token1;
 
     constructor(address _token0, address _token1) {
@@ -14,6 +21,7 @@ contract Fees {
         token1 = _token1;
     }
 
+    // We need to manually take care of the failure cases because we are doing a low level call.
     function _safeTransfer(
         address token,
         address to,
@@ -24,12 +32,25 @@ contract Fees {
         (bool success, bytes memory data) = token.call(
             abi.encodeWithSelector(IERC20.transfer.selector, to, amount)
         );
+        // Check that it returned the boolean true or no bytes or true in bytes
         require(
             success && (data.length == 0 || abi.decode(data, (bool))),
             "PairHelper: failed to transfer"
         );
     }
 
+    /**
+     * @dev The pair calls this function to transfer the fees accrued by LP providers. The amount is calculated in the pair.
+     *
+     * @param recipient The address that will receive the fees.
+     * @param amount0 Fees collected by token0 of the pair.
+     * @param amount1 Fees collected by the token1 of the pair.
+     *
+     * Requirements:
+     *
+     * - Only the pair can call this function.
+     * - 0 amounts will not revert, they will simply have no effect.
+     */
     function claimFor(
         address recipient,
         uint256 amount0,

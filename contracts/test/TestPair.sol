0e5adab3d976eed96532caffc32700d77da41352
José Cerqueira <jose@ilovemochi.com>
2022-10-05 21:09:29 +0100

    chore: update sol version (#15)

--- contracts/test/TestPair.sol
+++ contracts/test/TestPair.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity 0.8.17;
 
 contract PairGetAmountError {
     function getAmountOut(address, uint256) external pure returns (uint256) {
--- contracts/test/reentrancy/ERC20RMint.sol
+++ contracts/test/reentrancy/ERC20RMint.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.16;
+pragma solidity 0.8.17;
 
 interface IPair {
     function mint(address to) external returns (uint256 liquidity);
